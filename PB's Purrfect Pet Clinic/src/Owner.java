a646833fd4eff36303047f4e7f3aa85e8ef598d9
Alex Valentine <glow741@gmail.com>
2014-04-06 12:41:23 -0500

    Added base functionality for Pet and Owner classes.

--- PB's Purrfect Pet Clinic/src/Owner.java
+++ PB's Purrfect Pet Clinic/src/Owner.java
@@ -1,4 +1,67 @@
+import java.util.Vector;
+
 
 public class Owner {
-
+	final String FirstName;
+	final String LastName;
+	final String Address;
+	final String City;
+	final String State;
+	final String Zip;
+	
+	Vector<Pet> Pets;
+	
+	public Owner(String firstname, String lastname, String address, String city, String state, String zip){
+		this.FirstName = firstname;
+		this.LastName = lastname;
+		this.Address = address;
+		this.City = city;
+		this.State = state;
+		this.Zip = zip;
+		
+		this.Pets = new Vector<Pet>();
+	}
+	
+	public Owner(String firstname, String lastname, String address, String city, String state, String zip, Vector<Pet> pets){
+		this.FirstName = firstname;
+		this.LastName = lastname;
+		this.Address = address;
+		this.City = city;
+		this.State = state;
+		this.Zip = zip;
+		
+		this.Pets = pets;
+	}
+	
+	public String getFirstName(){
+		return this.FirstName;
+	}
+	
+	public String getLastName(){
+		return this.LastName;
+	}
+	
+	public String getAddress(){
+		return this.Address;
+	}
+	
+	public String getCity(){
+		return this.City;
+	}
+	
+	public String getState(){
+		return this.State;
+	}
+	
+	public String getZip(){
+		return this.Zip;
+	}
+	
+	public Vector<Pet> getPets(){
+		return this.Pets;
+	}
+	
+	public void addPet(Pet newPet){
+		this.Pets.add(newPet);
+	}
 }
--- PB's Purrfect Pet Clinic/src/Pet.java
+++ PB's Purrfect Pet Clinic/src/Pet.java
@@ -1,4 +1,101 @@
+import java.util.Date;
+
 
 public class Pet {
-
+	public static enum Type{
+		cat,
+		dog;
+	}
+	
+	public static enum Sex{
+		male,
+		female;
+	}
+	
+	public static enum Size{
+		small,
+		medium,
+		large;
+	}
+	
+	final Type Type;
+	final String Name;
+	final String OwnerName;
+	final Sex Sex;
+	final Size Size;
+	final String Color;
+	final Date DateOfBirth;
+	final String Prescriptions;
+	final Double Weight;
+	
+	public Pet(Type type, String name, String owner, Sex sex, Size size, String color, Date dob, String prescriptions, Double weight){
+		this.Type = type;
+		this.Name = name;
+		this.OwnerName = owner;
+		this.Sex = sex;
+		this.Size = size;
+		this.Color = color;
+		this.DateOfBirth = dob;
+		this.Prescriptions = prescriptions;
+		this.Weight = weight;
+	}
+	
+	public String getTypeString(){
+		if(this.Type == Type.cat)
+			return "Cat";
+		else
+			return "Dog";
+	}
+	
+	public Type getType(){
+		return this.Type;
+	}
+	
+	public String getName(){
+		return this.Name;
+	}
+	
+	public String getOwnerName(){
+		return this.OwnerName;
+	}
+	
+	public String getSexString(){
+		if(this.Sex == Sex.female)
+			return "Female";
+		else
+			return "Male";
+	}
+	
+	public Sex getSex(){
+		return this.Sex;
+	}
+	
+	public String getSizeString(){
+		if(this.Size == Size.small)
+			return "Small";
+		else if(this.Size == Size.medium)
+			return "Medium";
+		else
+			return "Large";
+	}
+	
+	public Size getSize(){
+		return this.Size;
+	}
+	
+	public String getColor(){
+		return this.Color;
+	}
+	
+	public Date getDOB(){
+		return this.DateOfBirth;
+	}
+	
+	public String getPrescriptions(){
+		return this.Prescriptions;
+	}
+	
+	public Double getWeight(){
+		return this.Weight;
+	}
 }
